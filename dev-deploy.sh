d091d192a3679cb4b7570ff90164caada00a9e61
Dan Stadler <danstadler@janrain.com>
2011-02-18 09:46:08 -0800

    updates to dev-deploy

--- dev-deploy.sh
+++ dev-deploy.sh
@@ -1,6 +1,6 @@
 # design directories, may not exist on Magento when this script is run
-default_layout=$MAGENTO_HOME/app/design/frontend/default/default/layout/
-default_template=$MAGENTO_HOME/app/design/frontend/default/default/template/
+default_layout=$MAGENTO_1d5/app/design/frontend/default/default/layout/
+default_template=$MAGENTO_1d5/app/design/frontend/default/default/template/
 
 # create them if they don't
 if [ -d $default_layout ]; then
@@ -18,10 +18,10 @@ else
 fi
 
 # symlink everything we need from the project into the correct Magento dirs.
-ln -f -s $MAGENTO_CODE_HOME/app/etc/modules/Janrain_Engage.xml $MAGENTO_HOME/app/etc/modules/
-ln -f -s $MAGENTO_CODE_HOME/app/code/local/Janrain $MAGENTO_HOME/app/code/local/
-ln -f -s $MAGENTO_CODE_HOME/app/design/frontend/default/default/layout/* $MAGENTO_HOME/app/design/frontend/default/default/layout/
-ln -f -s $MAGENTO_CODE_HOME/app/design/frontend/default/default/template/* $MAGENTO_HOME/app/design/frontend/default/default/template/
-ln -f -s $MAGENTO_CODE_HOME/app/locale/en_US/* $MAGENTO_HOME/app/locale/en_US/
+ln -f -s $MAGENTO_1d5_PLUGIN/app/etc/modules/Janrain_Engage.xml $MAGENTO_1d5/app/etc/modules/
+ln -f -s $MAGENTO_1d5_PLUGIN/app/code/local/Janrain $MAGENTO_1d5/app/code/local/
+ln -f -s $MAGENTO_1d5_PLUGIN/app/design/frontend/default/default/layout/* $MAGENTO_1d5/app/design/frontend/default/default/layout/
+ln -f -s $MAGENTO_1d5_PLUGIN/app/design/frontend/default/default/template/* $MAGENTO_1d5/app/design/frontend/default/default/template/
+ln -f -s $MAGENTO_1d5_PLUGIN/app/locale/en_US/* $MAGENTO_1d5/app/locale/en_US/
 
 
