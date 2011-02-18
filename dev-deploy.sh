de1fcf035dcf9f4780c7b9378ed134cd42310251
Bryce Hamrick <bryce@janrain.com>
2011-02-17 17:03:56 -0800

    shell script symlinks

---
+++ dev-deploy.sh
@@ -0,0 +1,5 @@
+ln -f -s $MAGENTO_CODE_HOME/app/etc/modules/Janrain_Engage.xml $MAGENTO_HOME/app/etc/modules/
+ln -f -s $MAGENTO_CODE_HOME/app/code/local/Janrain $MAGENTO_HOME/app/code/local/
+ln -f -s $MAGENTO_CODE_HOME/app/design/frontend/default/default/layout/* $MAGENTO_HOME/app/design/frontend/default/default/layout/
+ln -f -s $MAGENTO_CODE_HOME/app/design/frontend/default/default/template/* $MAGENTO_HOME/app/design/frontend/default/default/template/
+ln -f -s $MAGENTO_CODE_HOME/app/locale/en_US/* $MAGENTO_HOME/app/locale/en_US/
