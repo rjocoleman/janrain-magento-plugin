bb0ef9091309376141f5d9bb5768c87cdcc7dc53
Dan Stadler <danstadler@janrain.com>
2011-02-23 17:15:53 -0800

    setting up locally

---
+++ app/code/local/Janrain/Engage/Helper/Data.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dan
+ * Date: Feb 23, 2011
+ * Time: 4:54:35 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+class Janrain_Engage_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
+
+
+?>
