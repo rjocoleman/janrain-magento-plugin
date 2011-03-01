94fc80fc79e1fd4df643debdcf6b712b24dd94cf
Dan Stadler <danstadler@janrain.com>
2011-03-01 11:52:38 -0800

    watir-testing

--- watir-testing/tests/sanity/sanity1.rb
+++ watir-testing/tests/sanity/sanity1.rb
@@ -5,19 +5,19 @@ require 'uri'
 
 # http://groups.google.com/group/watir-general/browse_thread/thread/26486904e89340b7?pli=1
 def try_url url
-  url = URI.parse(url) 
+  url = URI.parse(url)
   http = Net::HTTP.new(url.host, url.port)
 
-    http.start do
-      http.request_get(url.path.empty? ? "/" : url.path) do |res|
+  http.start do
+    http.request_get(url.path.empty? ? "/" : url.path) do |res|
 
-        return res
+      return res
 
-        # return false unless res.kind_of?(Net::HTTPSuccess)
-        # or Net::HTTPOK if you want to exclude 201, 202, 203, 204, 205 and 206
+      # return false unless res.kind_of?(Net::HTTPSuccess)
+      # or Net::HTTPOK if you want to exclude 201, 202, 203, 204, 205 and 206
 
-      end
     end
+  end
 
 end
 
@@ -27,11 +27,11 @@ puts "sanity1 tests..."
 
 # see if the Magento index page is serving
 begin
-    puts "trying #{ENV['MGP_ROOT']}"
-    result = try_url "#{ENV['MGP_ROOT']}"
-    puts "result: #{result.class}"
+  puts "trying #{ENV['MGP_ROOT']}"
+  result = try_url "#{ENV['MGP_ROOT']}"
+  puts "result: #{result.class}"
 rescue Exception => e
-    raise "caught exception: #{e}"
+  raise "caught exception: #{e}"
 end
 
 puts "done with sanity1 tests."
