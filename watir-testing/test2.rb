8204ea07f0d31749e18fab175e730f7bc327b215
Dan Stadler <danstadler@janrain.com>
2011-03-01 16:06:43 -0800

    watir-testing

--- watir-testing/test2.rb
+++
@@ -1,9 +0,0 @@
-require 'watir/ie'
-
-def is_element_subclass? klass
-  while klass = klass.superclass
-    return true if klass == Watir::Element
-  end
-end
-
-ObjectSpace.each_object(Class){|c| puts c if is_element_subclass?(c)}
