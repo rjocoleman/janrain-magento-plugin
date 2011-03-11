84efa6a2714298447917ee489e7151dedae4f641
Bryce Hamrick <bryce@janrain.com>
2011-03-11 15:53:14 -0800

    removed test files

--- watir-testing/setup_teardown/dba/drop_magento_db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# name of mysql program - stick it in a var so it's easy to concatenate with path
-m=mysql
-
-# drop all Magento tables
-"$MGP_MYSQL_PATH$m" --user=$MGP_DB_USERNAME --password=$MGP_DB_PASSWORD -BNe "show tables" $MGP_DB_NAME | tr '\n' ',' | sed -e 's/,$//' | awk '{print "SET FOREIGN_KEY_CHECKS = 0;DROP TABLE IF EXISTS " $1 ";SET FOREIGN_KEY_CHECKS = 1;"}' | "$MGP_MYSQL_PATH$m" --user=$MGP_DB_USERNAME --password=$MGP_DB_PASSWORD $MGP_DB_NAME
-
--- watir-testing/setup_teardown/dba/load_fresh_install_db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# name of mysql program - stick it in a var so it's easy to concatenate with path
-m=mysql
-
-# reload sample Magento data
-"$MGP_MYSQL_PATH$m" --user=$MGP_DB_USERNAME --password=$MGP_DB_PASSWORD $MGP_DB_NAME < ./magento_fresh_install.sql > fresh_install_result.txt
-
