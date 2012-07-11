6405cbfa266ad7f0a6ef2e50ed603ce0b5fa5d6d
Jeremy Bradbury <jeremy@janrain.com>
2012-07-11 16:53:31 -0700

    +support for multiple share buttons with getters and setters

--- app/code/community/Janrain/Engage/Block/Share.php
+++ app/code/community/Janrain/Engage/Block/Share.php
@@ -1,6 +1,38 @@
 <?php
 
 class Janrain_Engage_Block_Share extends Mage_Core_Block_Abstract implements Mage_Widget_Block_Interface {
+    protected $url   = 'this';
+    protected $title = 'this';
+    protected $desc  = 'this';
+
+    protected function _setUrl($var)         {
+        $this->url = $var;
+    }
+    protected function _setTitle($var)       {
+        $this->title = $var;
+    }
+    protected function _setDescription($var) {
+        $this->desc = $var;
+    }
+    protected function _getUrl() {
+        if($this->url == 'this') {
+            return "document.location.href";
+        }
+        return "'$this->url'";
+    }
+    protected function _getTitle() {
+        if($this->title == 'this') {
+            return "document.title";
+        }
+        return "'$this->title'";
+    }
+    protected function _getDescription() {
+        if($this->desc == 'this') {
+            return "document.getElementsByName('description')[0].getAttribute('content')";
+        }
+        return "'$this->desc'";
+    }
+
 
     public function rpx_social_icons() {
         $social_pub = Mage::getStoreConfig('engage/vars/socialpub');
@@ -24,9 +56,11 @@ class Janrain_Engage_Block_Share extends Mage_Core_Block_Abstract implements Mag
     protected function _toHtml() {
         $link = '';
 
+        $onclick = "setShare({$this->_getUrl()}, {$this->_getTitle()}, {$this->_getDescription()});";
+
         if ($icons = $this->rpx_social_icons()) {
-            $link .= '<div class="rpxsocial rpx_tooltip" id="janrainEngageShare">';
-            $link .= '<span class="rpxsharebutton">share</span><div class="rpx_share_tip">Share this on:<br />' . $icons . '</div></div>';
+            $link .= '<div class="rpxsocial rpx_tooltip">';
+            $link .= '<span class="rpxsharebutton" onClick="'.$onclick.'">share</span><div class="rpx_share_tip">Share this on:<br />' . $icons . '</div></div>';
         }
 
         return $link;
@@ -35,9 +69,9 @@ class Janrain_Engage_Block_Share extends Mage_Core_Block_Abstract implements Mag
     protected function _prepareLayout() {
         if ($this->getLayout()->getBlock('janrain_engage_share') == false) {
             $block = $this->getLayout()
-                ->createBlock('core/template', 'janrain_engage_share')
-                ->setData('message', $this->getShareText())
-                ->setTemplate('janrain/engage/share.phtml');
+            ->createBlock('core/template', 'janrain_engage_share')
+            ->setData('message', $this->getShareText())
+            ->setTemplate('janrain/engage/share.phtml');
             $this->getLayout()->getBlock('before_body_end')->insert($block);
         }
 
